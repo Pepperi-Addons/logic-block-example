168beee21c21fcbce30551b7b06b733ebebde0cf
roiaharon-pepperi <roi.a@pepperi.com>
2022-08-14 17:50:20 +0300

    Added missing module

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApplicationRef, Component, DoBootstrap, Injector, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LogicBlockModule } from './logic-block/logic-block.module';
@@ -20,6 +21,7 @@ const routes: Routes = [
 
 @NgModule({
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
         LogicBlockModule,
         RouterModule.forRoot(routes),
